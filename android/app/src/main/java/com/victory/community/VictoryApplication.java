0c66a2a9e88a2c5d8ada328b0fe0e5e9134ac775
Usermongkay <santricom75@gmail.com>
2025-07-26 16:18:58 +0700

    Add dumy

---
+++ android/app/src/main/java/com/victory/community/VictoryApplication.java
@@ -0,0 +1,12 @@
+package com.victory.community;
+
+import android.app.Application;
+
+public class VictoryApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Dummy Application Class
+    }
+}
+
